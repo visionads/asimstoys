784071f57f62baa0ff2733d3679a9082dd88ff57
SR <me@selimreza.com>
2016-07-23 21:07:52 +0600

    updated

--- app/modules/web/Views/product/details.blade.php
+++ app/modules/web/Views/product/details.blade.php
@@ -122,7 +122,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div style="width: 100%; height: 180px;  overflow: scroll; display: block; border-bottom: 1px solid #efefef; ">
+                                            <div style="width: 100%; height: 180px;  overflow: scroll; display: block; border-bottom: 1px solid #efefef; padding-bottom: 10px">
                                                 <?php echo $product->short_description; ?>
 
                                             </div>
