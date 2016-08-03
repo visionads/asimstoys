3a307b68c7442f24bd19d05f53250ddb241c3220
SR <me@selimreza.com>
2016-08-03 22:59:08 +0600

    up

--- app/modules/web/Controllers/OrderController.php
+++ app/modules/web/Controllers/OrderController.php
@@ -479,15 +479,13 @@ class OrderController extends Controller
             }
 
             //send email
-            $to_email = $customer->email;
-            $to_name = $customer->first_name." ". $customer->last_name;
-
-            $subject = "Approved Payment of invoice # ".$invoice_no. " | Asims Toys ";
-            $body = "Dear ".$to_name. " Your Payment is approved !";
-
+            $to_email = $user_data->email;
+            $to_name = $user_data->first_name." ". $user_data->last_name;
+            $subject = "Order placed  # ".$order_head->invoice_no. " | Asims Toys ";
+            $body = "Dear ".$user_data->first_name. " Your Order is placed !";
             $mail = SendMailer::send_mail_by_php_mailer($to_email, $to_name, $subject, $body);
 
-            
+
         }else{
             Session::flash('flash_message_error', "No Product is available in cart");
             return redirect()->route('mycart');
