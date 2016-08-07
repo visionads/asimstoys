923b1b4fcefd31c55a74e30e23075046948250f0
SR <me@selimreza.com>
2016-08-07 19:28:20 +0600

    UP

--- app/modules/web/Views/accounts/order_details.blade.php
+++ app/modules/web/Views/accounts/order_details.blade.php
@@ -26,87 +26,42 @@
 
             </div>
 
-            @foreach($order as $order)
+            
 
-                <div class="invoice-header">
-                    <div class="col-md-3">
-                        <h3>
-                            <div>
-                                <small><strong>Asims</strong>Toys</small><br>
-                                {{$order->invoice_no}}
-                            </div>
-                        </h3>
-                        <div>
-                            <small><strong>Date</strong></small><br>
-                            {{$order->created_at}}
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <address>
-                            Asims Toys.<br>
-                            Australia <br>
-                            AU
-                        </address>
-                    </div>
 
-                    <div class="col-md-6 pull-right">
-
-                        <div class="col-md-12">
-                            <div class="pull-right">
-                                <h2 style="color: darkblue">Total Amount : {{@$total_amount->total_amount}}</h2>
-                                <h2 style="color: green">Paid Amount : {{@$paid_amount->paid_amount }}</h2>
-                                <h2 style="color: red">Due Amount : {{@$due_amount}}</h2>
-                            </div>
-                        </div>
-                        @if(@$total_amount->total_amount != @$paid_amount->paid_amount)
-                            <div class="col-md-12 pull-right" style="padding: 20px">
-                                <a href="{{ route('lay_by_pay_option', ['order_head_id'=>$order_head_id]) }}" class="cart-checkout" style="width: 55%; text-align: center; box-shadow: 3px 3px 3px #888888">Want to Pay Now ? </a>
-
-                            </div>
-                        @endif
-
-                    </div>
-                </div> <!-- / .invoice-header -->
-
-                <p> &nbsp; </p>
-                <p> &nbsp; </p>
-                <p> &nbsp; </p>
-
-                <h3 style="color: green;">Invoice History </h3>
+            @if($order_pay_trn)
+                <h3 style="color: green;">Payment History </h3>
                 <table class="table table-striped cart-table">
                     <thead>
                     <tr>
-                        <td>Order Invoice </td>
-                        <td>Product Name (ID)</td>
-                        <td>Variation </td>
-                        <td>Quantity</td>
-                        <td>Color</td>
-                        <td>Background Color</td>
-                        <td>Plate Text</td>
-                        <td>Price</td>
+                        <td>Invoice No # </td>
+                        <td>Customer Name </td>
+                        <td>Payment Type </td>
+                        <td>Amount </td>
+                        <td>Date </td>
+                        <td>Transactio No# </td>
+                        <td>Gateway Name </td>
+                        <td>Status </td>
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach($order->relOrderDetail as $order_dt)
+                    @foreach($order_pay_trn as $values)
                         <tr>
-                            <td>{{ \App\OrderHead::findOrFail($order_dt->order_head_id)->invoice_no}}</td>
-                            <td>{{\App\Product::findOrFail($order_dt->product_id)->title}}</td>
-                            <td>{{ $order_dt->product_variation_id}}</td>
-                            <td>{{$order_dt->qty}}</td>
-                            <td>{{@$order_dt->product_variation_id}}</td>
-                            <td>{{@$order_dt->background_color}}</td>
-                            <td>{{@$order_dt->plate_text}}</td>
-                            <td>{{$order_dt->price}}</td>
+                            <td>{{ \App\OrderHead::findOrFail($values->order_head_id)->invoice_no}}</td>
+                            <td>{{\App\Customer::findOrFail($values->customer_id)->first_name}}</td>
+                            <td>{{ $values->payment_type }}</td>
+                            <td>{{ $values->amount  }}</td>
+                            <td>{{ $values->date }}</td>
+                            <td>{{ $values->transaction_no }}</td>
+                            <td>{{ $values->gateway_name }}</td>
+                            <td>{{ $values->status }}</td>
                         </tr>
                     @endforeach
 
                     </tbody>
                 </table>
-                <p class="pull-right">Total Amount {{$total_amount->total_amount}}</p>
-            @endforeach
-
-
-            
+                <p class="pull-right">Paid Amount {{@$paid_amount->paid_amount}}</p>
+            @endif
 
             <div class="col-md-12 margin-top-30 margin-bottom-30">
                 <div class="col-md-6">
