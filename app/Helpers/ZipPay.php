8ed451bfd924c799a542dd3eda539fff18190015
Selim Reza <me@selimreza.com>
2016-11-20 08:10:25 +0600

    zippay

--- app/Helpers/ZipPay.php
+++ app/Helpers/ZipPay.php
@@ -151,12 +151,11 @@ class ZipPay
 
         try{
             $response = $checkout->process();
-
+            $arr_response = @$response->toArray();
+            $url = @$arr_response['redirect_url'];
+            
             if($response->isSuccess())
             {
-                $arr_response = $response->toArray();
-                $url = $arr_response['redirect_url'];
-
                 //Do Something
                 header('Location: '.$url);
 
