099890757d052522c90c0e484552712d2ad00e73
mithunadhiakry <mithun.cse521@gmail.com>
2017-02-08 23:08:26 -0700

    Left amount & left amount is reverse in admin area

--- resources/views/order_payment/lay_by_order_details.blade.php
+++ resources/views/order_payment/lay_by_order_details.blade.php
@@ -72,13 +72,14 @@
                             <div class="pull-right"> <b>{{@$order_data[0]->freight_amount}} </b></div>
 						</div>
 						<div class="clearfix">
-							<div class="pull-left"> TOTAL Amount : </div>
-                            <div class="pull-right"> <b>{{@$order_data[0]->net_amount}} </b></div>
-						</div>
-						<div class="clearfix">
 							<div class="pull-left"> Left Amount : </div>
                             <div class="pull-right" style="color: red"> <b>{{@$due_amount}} </b></div>
 						</div>
+						<div class="clearfix">
+							<div class="pull-left"> TOTAL Amount : </div>
+                            <div class="pull-right"> <b>{{@$order_data[0]->net_amount}} </b></div>
+						</div>
+						
 						<div class="clearfix">
 							<div class="pull-left"> PAID Amount : </div>
                             <div class="pull-right" style="color: green;"> <b>{{@$paid_amount->paid_amount}}</b> </div>
